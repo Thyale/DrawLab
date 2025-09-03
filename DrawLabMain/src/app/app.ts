096e0cce082ed3c26d44105ed4fecaf0171bb385
Thyale MD <thyaledantas@gmail.com>
2025-09-03 16:23:43 -0300

    criação do componente card

--- DrawLabMain/src/app/app.ts
+++ DrawLabMain/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/header/header';
+import { Card } from "./componentes/card/card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Card],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
