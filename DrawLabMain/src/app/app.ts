f81203dfae3c191481abbd87437a85578d6afb1e
Thyale MD <thyaledantas@gmail.com>
2025-09-05 11:29:29 -0300

    Criação do componente "programas"

--- DrawLabMain/src/app/app.ts
+++ DrawLabMain/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/header/header';
 import { Card } from "./componentes/card/card";
+import { Programas } from './componentes/programas/programas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Card],
+  imports: [RouterOutlet, Header, Card, Programas],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
