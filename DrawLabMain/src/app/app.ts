f291953261f8fedb59e071c65b7ed69695ea1f53
Thyale MD <thyaledantas@gmail.com>
2025-09-17 15:35:20 -0300

    Ajustes e criação da sessão "depoimentos"

--- DrawLabMain/src/app/app.ts
+++ DrawLabMain/src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './componentes/header/header';
 import { Card } from "./componentes/card/card";
 import { Programas } from './componentes/programas/programas';
 import { Sobre } from './componentes/sobre/sobre';
+import { Depoimentos } from "./componentes/depoimentos/depoimentos";
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Card, Programas, Sobre],
+  imports: [Header, Card, Programas, Sobre, Depoimentos],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
